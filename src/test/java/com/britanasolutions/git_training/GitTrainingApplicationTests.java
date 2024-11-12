fb099bdf8d8c52687a56da34aee3ffbd0b9d87df
jdait <jefferson.dait@britanasolutions.com>
2024-11-12 17:49:07 +0800

    initial code

---
+++ src/test/java/com/britanasolutions/git_training/GitTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.britanasolutions.git_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
